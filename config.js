5e2a8c7d050ba64d2921e47d53cd1b2d5cf3a680
Olaf Toebes <olaf.toebes@hva.nl>
2026-02-14 03:12:03 +0100

    public mp3 link

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "愛人錯過-告五人.mp3",      // Music streaming URL
+        musicUrl: "https://olaf-hva.github.io/Megan-Valentine-2026/愛人錯過-告五人.mp3",      // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
